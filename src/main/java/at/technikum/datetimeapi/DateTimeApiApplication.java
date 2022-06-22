e72c650bc0829d4d39aa0afedd721a980933cd7c
DESKTOP-05Q6GBI\Nadine <wi21b045@technikum-wien.at>
2022-06-22 19:46:16 +0200

    Initial commit

---
+++ src/main/java/at/technikum/datetimeapi/DateTimeApiApplication.java
@@ -0,0 +1,13 @@
+package at.technikum.datetimeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DateTimeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DateTimeApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/technikum/datetimeapi/DateTimeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package at.technikum.datetimeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DateTimeApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
